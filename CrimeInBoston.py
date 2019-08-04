76e9faaa6445193e32735936ce97dbcb1176f900
Milesh Shah <mas2g2@mail.missouri.edu>
2019-08-03 21:31:39 -0500

    Add files via upload

--- CrimeInBoston.py
+++ CrimeInBoston.py
@@ -14,8 +14,8 @@ changes_in_crime = []
 with open("changes_in_crime.csv","w") as f:
     f.write("YearMonth,Change in crime\n")
     for i in range(1,40):
-        changes_in_crime.append([i,float(data[i-1,1])-float(data[i,1])])
-        f.write(data[i,0]+ ","+str(float(data[i-1,1])-float(data[i,1]))+"\n")
+        changes_in_crime.append([i,-float(data[i-1,1])+float(data[i,1])])
+        f.write(data[i,0]+ ","+str(-float(data[i-1,1])+float(data[i,1]))+"\n")
     f.close()
 changes_in_crime = np.array(changes_in_crime)
 plt.title("Changes in Crime Boston")
